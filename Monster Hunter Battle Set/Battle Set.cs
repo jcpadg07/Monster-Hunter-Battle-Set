40e145e5535941be3df80485d6ec314c7b6f072b
James Padgett <jampad14@gmail.com>
2019-02-20 19:37:03 -0500

    First commit

---
+++ Monster Hunter Battle Set/Battle Set.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monster_Hunter_Battle_Set
+{
+    class Battle_Set
+    {
+    }
+}
